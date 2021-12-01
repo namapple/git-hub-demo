2cd1f51e0952f5536a216284c0e2d20293863bd4
Nam Dao <bk.namapple@gmail.com>
2021-12-01 20:22:40 +0700

    add a function

--- Array.c
+++ Array.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
+void greeting(){
+
+    printf("Hello!\n");
+}
+
 int main(){
     int arr[10] = {1,2,3,4,5,6,7,8,9,10};
-    printf("Element at Index 0 is %d", arr[0]);
-    printf("Element at Index 1 is %d", arr[1]);
-    printf("Element at Index 2 is %d", arr[2]);
-    printf("Element at Index 3 is %d", arr[3]);
-    printf("Element at Index 4 is %d", arr[4]);
+    printf("Element at Index 0 is %d\n", arr[0]);
+    printf("Element at Index 1 is %d\n", arr[1]);
+    printf("Element at Index 2 is %d\n", arr[2]);
+    printf("Element at Index 3 is %d\n", arr[3]);
+    printf("Element at Index 4 is %d\n", arr[4]);
+
+    greeting();
+
     return 0;
 }
