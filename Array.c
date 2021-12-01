012fba2eb59bd9165b36b047c0d75d74394011aa
Nam Dao <bk.namapple@gmail.com>
2021-12-01 10:44:50 +0700

    add more contents

--- Array.c
+++ Array.c
@@ -4,5 +4,6 @@ int main(){
     int arr[10] = {1,2,3,4,5,6,7,8,9,10};
     printf("Element at Index 0 is %d", arr[0]);
     printf("Element at Index 1 is %d", arr[1]);
+    printf("Element at Index 2 is %d", arr[2]);
     return 0;
 }
