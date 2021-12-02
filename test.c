22e4c17fa44004114596aee02a432f5e79c774d4
Nam Dao <bk.namapple@gmail.com>
2021-12-02 09:49:22 +0700

    add two variables

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-
+    int a, b;
     printf("%s", "The second file is already modified.\n");
     printf("%s", "Let see how it's done.\n");
     printf("%s", "Test cloning git repositoty.\n");
